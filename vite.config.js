96755cabaaf58a627d4f5d99c2199a695d9003e3
WROTZ <94730219+WROTZ@users.noreply.github.com>
2025-07-12 00:25:51 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// ✅ Replace with your repo name below (clinic-calendar2)
-export default defineConfig({
-  plugins: [react()],
-  base: '/clinic-calendar2/' // VERY IMPORTANT
-})
